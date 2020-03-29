1ebdbe95149bc421a7b92b431f452ec6eeb0fdc2
Lotrmay <lotrmay@gmail.com>
2020-03-30 01:07:19 +0200

    Update forfun.py

--- forfun.py
+++ forfun.py
@@ -1,12 +1,12 @@
-
+#small python function which can tell you which numbers are narcissistic more on that here:https://twitter.com/fermatslibrary/status/1243532484991684615
 def narcissisticNumbers(numOfDigits):
     ret=[]
     length=len(str(numOfDigits))
     for x in range(numOfDigits,numOfDigits*10):
-        cislo=0
-        for cislice in range(length):
-            cislo+=pow(int(str(x)[cislice]),length)
-        if(cislo==x):
+        number=0
+        for digit in range(length):
+            number+=pow(int(str(x)[digit]),length)
+        if(number==x):
             ret.append(x)
     print(ret)
         
